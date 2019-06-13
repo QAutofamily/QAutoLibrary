1f19a861300e8a58ee8e23a4a6a9eae006173985
pekkaan <anssi.pekkarinen@qautomate.fi>
2019-06-13 11:29:40 +0300

    Parsestack parsing for time stack livemonitor analyzes

---
+++ QAutoLibrary/ParseStack.py
@@ -0,0 +1,24 @@
+from QAutoLibrary.JsonLogger import JsonLogger
+from datetime import datetime
+from xml.dom import minidom
+import os
+
+xmldoc = minidom.parse(os.path.join('test_reports', 'output.xml'))
+itemlist = xmldoc.getElementsByTagName('test')
+kwlist = xmldoc.getElementsByTagName('kw')
+
+JsonLogger().json_logger_init("time_stack.json", itemlist[0].attributes['name'].value)
+FMT = '%Y%m%d %H:%M:%S.%f'
+for s in kwlist:
+    if s.attributes['name'].value == "Teardown":
+        break
+    elif "suite" in s.attributes['name'].value.lower() or "log" in s.attributes['name'].value.lower() or "setup" in \
+            s.attributes['name'].value.lower():
+        pass
+    else:
+        s1 = s.getElementsByTagName("status")[0].attributes['starttime'].value
+        s2 = s.getElementsByTagName("status")[0].attributes['endtime'].value
+
+        tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+        JsonLogger().append_to_jsondoc("time_stack.json", itemlist[0].attributes['name'].value,
+                                       s.attributes['name'].value, str(tdelta)[:-3])
