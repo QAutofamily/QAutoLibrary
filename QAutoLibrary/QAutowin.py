4a0b0d68aa6beb0c8ae7d09658a47f86ec677add
LasseMatikainen <lasse.matikainen@q-factory.fi>
2020-03-23 17:22:10 +0200

    Window fix and documentation fix

--- QAutoLibrary/QAutowin.py
+++ QAutoLibrary/QAutowin.py
@@ -6,6 +6,8 @@ import time
 import os
 from PIL import ImageGrab
 import subprocess
+import pygetwindow as gw
+import win32api
 
 # http://robotframework.org/robotframework/latest/RobotFrameworkUserGuide.html#testing-libraries
 # https://github.com/robotframework/robotframework/blob/master/doc/userguide/src/ExtendingRobotFramework/CreatingTestLibraries.rst
@@ -15,7 +17,8 @@ class QAutowin(object):
 
     def __init__(self, backend="uia"):
         self.app = pywinauto.application.Application(backend=backend)
-        pass
+        self.backend = backend
+        self.window = None
 
     @keyword(name='Open Application')
     def Open_Application(self, appname, **kwargs):  # arg=application, arg2=backend Win32 API or MS UI Automation
@@ -40,7 +43,6 @@ class QAutowin(object):
             | Connect application  best_match=Untitled - Notepad
         """
         for kwarg in kwargs.keys():
-            print(kwarg)
             if kwarg == "process":
                 self.app.application_process = kwargs["process"]
                 self.app.connect(process=int(self.app.application_process))
@@ -48,7 +50,26 @@ class QAutowin(object):
                 self.app.application_title = kwargs[list(kwargs.keys())[-1]]
                 self.app.connect(**kwargs)
             logger.info(self.app.process)
+
+            windows_list = gw.getWindowsWithTitle(self.app.application_title)
+
+            hwnd = str(windows_list).split("hWnd=")[1].split(")")[0]
+            app = pywinauto.application.Application(backend="uia")
+            pyapp = app.connect(handle=int(hwnd.split("hwnd=")[-1]))
+            window_count = len(pyapp.windows())
+            self.window = pyapp.window(found_index=(window_count - 1))
+
             return self.app.process
+
+    def find_connected_app_window(self):
+        """
+        **Returns window of connected app**
+
+        --------------
+        :Example:
+            | ${window}=  Find app window
+        """
+        return self.window
 
     @keyword(name='Click Element')
     def Click_Element(self, **kwargs):
@@ -60,46 +81,59 @@ class QAutowin(object):
         :Example:
             | Click element  title=File
         """
+        window = self.find_connected_app_window()
         if "text" in kwargs:
             logger.info('Clicking element %s.' % kwargs)
             for kwarg in kwargs.keys():
                 if kwarg == "text":
-                    self.app.window(title_re=self.app.application_title)[(kwargs["text"])].wait('visible', timeout=10)
-                    self.app.window(title_re=self.app.application_title)[(kwargs["text"])].click_input()
+                    window[(kwargs["text"])].wait('visible', timeout=10)
+                    window[(kwargs["text"])].click_input()
                     break
         elif 'x' in kwargs and 'y' in kwargs:
             logger.info('Clicking at coordinates %s.' % kwargs)
             self.Click_Coordinates(**kwargs)
         else:
             logger.info('Clicking element %s.' % kwargs)
-            self.app.window(title_re=self.app.application_title).child_window(**kwargs).wait('visible', timeout=10)
-            self.app.window(title_re=self.app.application_title).child_window(**kwargs).click_input()
+            window.child_window(**kwargs).wait('visible', timeout=10)
+            window.child_window(**kwargs).click_input()
 
     @keyword(name='Send Keywords')  # Send input data. Useful for text fields, that "Input text" does not recognize. Also, you can send keyboard actions, for example like ~ for Enter
     # https://pywinauto.readthedocs.io/en/latest/code/pywinauto.keyboard.html
     def Send_Keywords(self, user_input):
+        """
+        **Send key presses**
+
+        --------------
+        :Example:
+            | Send_Keywords  x
+        """
         logger.info('Send keywords %s.' % user_input)
         send_keys(user_input)
 
     @keyword(name='Input Text')
     def Input_Text(self, user_input, **kwargs):
+        """
+        **Input text to element**
+
+        :kwargs: auto_id, class_name, class_name_re, title, title_re, control_type
+        --------------
+        :Example:
+            | Input Text  text  title=File
+        """
+        window = self.find_connected_app_window()
         # Input text by: auto_id, class_name, class_name_re, title, title_re, control_type
         if "text" in kwargs:
             logger.info('Input text %s element %s.' % (user_input, kwargs))
             for kwarg in kwargs.keys():
                 if kwarg == "text":
-                    self.app.window(title_re=self.app.application_title)[(kwargs["text"])].wait('visible', timeout=10)
-                    self.app.window(title_re=self.app.application_title)[(kwargs["text"])].type_keys(user_input,
-                                                                                                     with_spaces=True,
-                                                                                                     with_tabs=True)
+                    window[(kwargs["text"])].wait('visible', timeout=10)
+                    window[(kwargs["text"])].type_keys(user_input, with_spaces=True, with_tabs=True)
                     break
         else:
             logger.info('Input text %s element %s.' % (user_input, kwargs))
-            self.app.window(title_re=self.app.application_title).child_window(**kwargs).wait('visible', timeout=10)
-            self.app.window(title_re=self.app.application_title).child_window(**kwargs).set_text("")
-            self.app.window(title_re=self.app.application_title).child_window(**kwargs).type_keys(user_input,
-                                                                                                  with_spaces=True,
-                                                                                                  with_tabs=True)
+            window.child_window(**kwargs).wait('visible', timeout=10)
+            window.child_window(**kwargs).set_text("")
+            window.child_window(**kwargs).type_keys(user_input, with_spaces=True, with_tabs=True)
 
     @keyword(name='Click Coordinates')
     def Click_Coordinates(self, **kwargs):
@@ -109,6 +143,7 @@ class QAutowin(object):
         :Example:
             | Click element  x=500    y=300
         """
+        window = self.find_connected_app_window()
         for kwarg in kwargs.keys():
             #application_title = (apptitle)
             # "File"
@@ -117,14 +152,21 @@ class QAutowin(object):
             if kwarg == "y":
                 y = kwargs["y"]
 
-        self.app.window(title_re=self.app.application_title).maximize()
+        window.maximize()
         time.sleep(1)
-        self.app.window(title_re=self.app.application_title).click_input(coords=((int(x)), (int(y))))
+        window.click_input(coords=((int(x)), (int(y))))
 
     @keyword(name="Verify Text")
     def Verify_text(self, user_input, **kwargs):
+        """
+        **Verify text of element**
+
+        :kwargs: auto_id, class_name, class_name_re, title, title_re, control_type
+        --------------
+        :Example:
+            | Verify text  text  title=File
+        """
         text = self.Get_text(**kwargs)
-        print("Get_text:", text)
         if text == user_input:
             logger.info("%s is equal to %s" % (text, user_input))
             pass
@@ -133,13 +175,22 @@ class QAutowin(object):
 
     @keyword(name="Get Text")
     def Get_text(self, **kwargs):
+        """
+        **Get text of element**
+
+        :kwargs: auto_id, class_name, class_name_re, title, title_re, control_type
+        --------------
+        :Example:
+            | {text}=  Get text  title=File
+        """
+        window = self.find_connected_app_window()
         if "text" in kwargs:
             for kwarg in kwargs.keys():
                 if kwarg == "text":
-                    text = self.app.window(title_re=".*"+self.app.application_title)[(kwargs["text"])].iface_value.CurrentValue
+                    text = window[(kwargs["text"])].iface_value.CurrentValue
                     return text
         else:
-            text = self.app.window(title_re=".*"+self.app.application_title).child_window(**kwargs).iface_value.CurrentValue
+            text = window.child_window(**kwargs).iface_value.CurrentValue
             return text
 
     @keyword(name='Right click element')
@@ -152,17 +203,18 @@ class QAutowin(object):
         :Example:
             | Right click element  title=File
         """
+        window = self.find_connected_app_window()
         if "text" in kwargs:
             logger.info('Clicking element %s.' % kwargs)
             for kwarg in kwargs.keys():
                 if kwarg == "text":
-                    self.app.window(title_re=self.app.application_title)[(kwargs["text"])].wait('visible', timeout=10)
-                    self.app.window(title_re=self.app.application_title)[(kwargs["text"])].right_click_input()
+                    window[(kwargs["text"])].wait('visible', timeout=10)
+                    window[(kwargs["text"])].right_click_input()
                     break
         else:
             logger.info('Clicking element %s.' % kwargs)
-            self.app.window(title_re=self.app.title).window(**kwargs).wait('visible', timeout=10)
-            self.app.window(title_re=self.app.title).window(**kwargs).right_click_input()
+            window.window(**kwargs).wait('visible', timeout=10)
+            window.window(**kwargs).right_click_input()
 
     @keyword(name='Close application')
     def Close_application(self):
@@ -176,17 +228,38 @@ class QAutowin(object):
 
     @keyword(name='Take screenshot')
     def Take_screenshot(self, error_image_folder):
+        """
+        **Take screenshot**
+
+        --------------
+        :Example:
+            | Take screenshot  screenshot_name
+        """
         image = ImageGrab.grab()
         image.save(error_image_folder)
         logger.error('Something went wrong. Screenshot taken')
 
     @keyword(name='Close all cmds')
     def Close_all_cmds(self):
+        """
+        **Close all cmds**
+
+        --------------
+        :Example:
+            | Close all cmds
+        """
         os.system("TASKKILL /F /IM cmd.exe")
 
     @keyword(name='Launch bat')
     def Launch_bat(self, directory, bat_name):
-        os.chdir(directory) # D:\\QAutomate\\QAutomate\\
+        """
+        **Run bat file**
+
+        --------------
+        :Example:
+            | Launch bat  directory  file_name
+        """
+        os.chdir(directory)
         subprocess.call('start"' + bat_name + '"', shell=True)
         time.sleep(3)
 
