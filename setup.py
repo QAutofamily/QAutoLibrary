70e3d712ec101d4c5a9f0943e62b742d43c17eca
Tony <72392290+tonyr-qf@users.noreply.github.com>
2021-03-02 08:30:46 +0200

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="QAutoLibrary",
-    version="0.0.10",
+    version="0.0.11",
     author="QAutomate",
     author_email="contact@qautomate.fi",
     description="QAutofamily testing framework library",
