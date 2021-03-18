206d7afe349c476e3b46755368a3c3cf27fce97e
Tony <72392290+tonyr-qf@users.noreply.github.com>
2021-03-18 08:03:11 +0200

    Update setup.py
    Version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="QAutoLibrary",
-    version="0.0.15",
+    version="0.0.16",
     author="QAutomate",
     author_email="contact@qautomate.fi",
     description="QAutofamily testing framework library",
